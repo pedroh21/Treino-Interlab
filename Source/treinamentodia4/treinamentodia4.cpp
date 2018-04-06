d41b25ca92da79333689e7da75aa76cebc32edd4
pedroh21 <pedroh.mss@usp.br>
2018-04-06 17:35:28 -0300

    porjeto dia 04/04/18

---
+++ Source/treinamentodia4/treinamentodia4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "treinamentodia4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, treinamentodia4, "treinamentodia4" );
---
+++ Source/treinamentodia4/treinamentodia4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "treinamentodia4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREINAMENTODIA4_API Atreinamentodia4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
