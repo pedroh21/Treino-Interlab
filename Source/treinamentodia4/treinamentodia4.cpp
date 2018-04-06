58e8b2df18e29961823b1081e4d01069c2ff3641
pedroh21 <pedroh.mss@usp.br>
2018-04-06 17:54:27 -0300

    Tetris_Start_Model

--- Source/treinamentodia4/treinamentodia4.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "treinamentodia4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, treinamentodia4, "treinamentodia4" );
